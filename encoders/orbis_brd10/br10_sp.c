a7a0a11b9283e3ff2c4adb3e0c5a79c2290ea906
Alex Tacescu <alextac98@gmail.com>
2020-01-04 17:21:37 -0800

    Add spi communication function to generalize between different responses and add skeleton responses to the .h file

--- encoders/orbis_brd10/br10_sp.c
+++ encoders/orbis_brd10/br10_sp.c
@@ -6,53 +6,35 @@
 #include "soft_spi.h"
 #include "br10_sp.h"
 
-// #define BYTE2_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c"
-// #define BYTE2_TO_BINARY(byte)  \
-//   (byte & 0x8000 ? '1' : '0'), \
-//   (byte & 0x4000 ? '1' : '0'), \
-//   (byte & 0x2000 ? '1' : '0'), \
-//   (byte & 0x1000 ? '1' : '0'), \
-//   (byte & 0x800 ? '1' : '0'), \
-//   (byte & 0x400 ? '1' : '0'), \
-//   (byte & 0x200 ? '1' : '0'), \
-//   (byte & 0x100 ? '1' : '0'), \
-//   (byte & 0x80 ? '1' : '0'), \
-//   (byte & 0x40 ? '1' : '0'), \
-//   (byte & 0x20 ? '1' : '0'), \
-//   (byte & 0x10 ? '1' : '0'), \
-//   (byte & 0x08 ? '1' : '0'), \
-//   (byte & 0x04 ? '1' : '0'), \
-//   (byte & 0x02 ? '1' : '0'), \
-//   (byte & 0x01 ? '1' : '0') 
-
-// #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-// #define BYTE_TO_BINARY(byte)  \
-//   (byte & 0x80 ? '1' : '0'), \
-//   (byte & 0x40 ? '1' : '0'), \
-//   (byte & 0x20 ? '1' : '0'), \
-//   (byte & 0x10 ? '1' : '0'), \
-//   (byte & 0x08 ? '1' : '0'), \
-//   (byte & 0x04 ? '1' : '0'), \
-//   (byte & 0x02 ? '1' : '0'), \
-//   (byte & 0x01 ? '1' : '0') 
+#define GEN_RESPONSE_LENGTH         3
+#define SERIAL_NO_RESPONSE_LENGTH   (GEN_RESPONSE_LENGTH + 6)
+#define SPEED_REQUEST_LENGTH        (GEN_RESPONSE_LENGTH + 2)
+#define TEMP_REQUEST_LENGTH         (GEN_RESPONSE_LENGTH + 2)
+#define DETAILED_STATUS_REQUEST     (GEN_RESPONSE_LENGTH + 1)
 
 gen_response br10_getGeneralResponse(){
-    uint8_t send_byte[3] = {0x00, 0x00, 0x00};
-    uint8_t receive_byte[3];
+    uint8_t send_bytes[3] = {0};
+    uint8_t receive_bytes[3];
 
-    spi_begin();
-    spi_delay(100); // t_S in documentation => time between CS and first CLK rising edge
-    spi_transfer(&receive_byte, &send_byte, 3);
-    spi_end();
-    spi_delay(100); // Pause time after CS is deactivated
+    spi_comms(&receive_bytes, &send_bytes, 3);
 
     gen_response response;
-    response.isError =   !(0b00000010 & receive_byte[1]);
-    response.isWarning = !(0b00000001 & receive_byte[1]);
+    response.isError =   !(0b00000010 & receive_bytes[1]);
+    response.isWarning = !(0b00000001 & receive_bytes[1]);
 
-    response.position =  ((uint16_t)((receive_byte[0] << 6) | 
-                                ((receive_byte[1] & 0b11111100) >> 2)))
+    response.position =  ((uint16_t)((receive_bytes[0] << 6) | 
+                                ((receive_bytes[1] & 0b11111100) >> 2)))
                                 * 360.0 / powf(2, 14);
-    response.crc = receive_byte[2];
+    response.crc = receive_bytes[2];
     return response;
+}
+
+
+
+static void spi_comms(uint8_t *receive_bytes, uint8_t *send_bytes, int length){
+    spi_begin();
+    spi_delay(100); // t_S in documentation => time between CS and first CLK rising edge
+    spi_transfer(receive_bytes, send_bytes, length);
+    spi_end();
+    spi_delay(100); // Pause time after CS is deactivated
 }
--- encoders/orbis_brd10/br10_sp.h
+++ encoders/orbis_brd10/br10_sp.h
@@ -10,30 +10,37 @@ typedef struct {
     char crc;
 } gen_response;
 
-struct serial_no_response {
+typedef struct {
     gen_response gen_response;
     char serial_no[6];
-};
+} serial_no_response;
 
-struct speed_request {
+typedef struct {
     gen_response gen_response;
     float speed;
-};
+} speed_response;
 
-struct temp_request {
+typedef struct {
     gen_response gen_response;
     float temp;
-};
+} temp_response;
 
-struct detailed_status {
+typedef struct {
     gen_response gen_response;
     bool isSignalAmplitudeHigh;
     bool isSignalAmplitudeLow;
     bool isTempOutOfRange;
     bool isSpeedHigh;
     bool isMultiturnError;
-};
+} detailed_status;
 
 gen_response br10_getGeneralResponse();
+serial_no_response br10_getSerialNoResponse();
+speed_response br10_getSpeedResponse();
+temp_response br10_getTempResponse();
+detailed_status br10_getDetailedStatusResponse();
+
+// Private Functions
+static void spi_comms(uint8_t *receive_bytes, uint8_t *send_bytes, int length);
 
 #endif // ORBIS_BR10_SP_H
